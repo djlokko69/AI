31d4f4278bc43fba207271b2c696cce8be54b93a
christapher.lockhar1 <christapher.lockhar1@600900B211DE410>
2018-03-08 15:49:32 +1100

    Added death and Spawn

--- Assets/~GoneHome/Scripts/Player.cs
+++ Assets/~GoneHome/Scripts/Player.cs
@@ -7,16 +7,23 @@ namespace GoneHome
     public class Player : MonoBehaviour
     {
         public float movementSpeed = 10f;
+        public float maxVelocity = 10f;
+        public GameObject deathParticales;
 
         private Rigidbody rigid;
-        private Transform cam; // << ADDED
+        private Transform cam; // << Camera ADDED
+
+        private Vector3 spawnPoint;
 
         // Use this for initialization
         void Start()
         {
             rigid = GetComponent<Rigidbody>();
 
-            cam = Camera.main.transform; //<<ADDED
+            cam = Camera.main.transform; //<< Camera ADDED
+
+            // Record starting position
+            spawnPoint = transform.position;
         }
 
         // Update is called once per frame
@@ -30,12 +37,39 @@ namespace GoneHome
             // Rotate input to face direction of Camera (flat on surface)
             inputDir = Quaternion.AngleAxis(cam.eulerAngles.y, Vector3.up) * inputDir;
 
+            /* OldCode: forces to move and Cant use with physics
             // Copy position
             Vector3 position = transform.position;
             // Offset to the new position
             position += inputDir * movementSpeed * Time.deltaTime;
             // Apply new position to rigidbody
             rigid.MovePosition(position);
+            */
+
+            //NewCode
+            // Add Force to Rigidbody
+            rigid.AddForce(inputDir * movementSpeed);
+            // Copy velocity into smaller variable
+            Vector3 vel = rigid.velocity;
+            // Check if vel's magnitude is greater than velocity
+            if (vel.magnitude > maxVelocity)
+            {
+                // Cap the Velocity
+                vel = vel.normalized * maxVelocity;
+            }
+            // Apply the new velocity to rigidbody
+            rigid.velocity = vel;
+
+        }
+
+        public void Reset()
+        {
+            // Spawn death particles where we reset
+            Instantiate(deathParticales, transform.position, transform.rotation);
+            // Reset position of the player to start position
+            transform.position = spawnPoint;
+            // Reset the Velocity
+            rigid.velocity = Vector3.zero;
         }
     }
 }
