4bd3816d4455f6f97e558fcd21b011ad2ac8e0f3
christapher.lockhar1 <christapher.lockhar1@600900B211DE410>
2018-05-24 15:42:48 +1000

    ~TowerDefense: Ai agent and MouseOrbit

---
+++ Unity/Assets/~TowerDefense/Scripts/Enemies/AIAgent.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+namespace TowerDefence
+{
+    public class AIAgent : MonoBehaviour
+    {
+        public Transform target;
+
+        private NavMeshAgent nav;
+
+        // Use this for initialization
+        void Start()
+        {
+            // get reference to nav mesh
+            nav = GetComponent<NavMeshAgent>();
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            // if there is a target
+            if (target)
+            {
+                nav.SetDestination(target.position);
+            }
+        }
+    }
+}
---
+++ Unity/Assets/~TowerDefense/Scripts/Enemies/Enemy.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace TowerDefense
+{
+    public class Enemy : MonoBehaviour
+    {
+        public float health = 100;
+        
+        public void DealDamage(float damage)
+        {
+            // Deal damage to enemy
+            health -= damage;
+            // If there is no health
+            if (health <= 0)
+            {
+                // Kill off the GameObject
+                Destroy(gameObject);
+            }
+        }
+    }
+}
